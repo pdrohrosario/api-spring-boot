ee0c212a1429edf5ad994b2d5d886b62859cd421
Pedro Rosário <pedro.rosario@neomind.com.br>
2022-08-06 19:07:17 -0300

    -Implementação do pageableDefault para TopicController.list

--- src/main/java/com/forum/controller/TopicController.java
+++ src/main/java/com/forum/controller/TopicController.java
@@ -8,15 +8,14 @@ import com.forum.model.dto.TopicDto;
 import com.forum.repository.CourseRepository;
 import com.forum.repository.TopicRepository;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -25,37 +24,43 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping("/topics")
 public class TopicController
 {
-	
+
 	@Autowired
 	private TopicRepository topicRepository;
-	
+
 	@Autowired
 	private CourseRepository courseRepository;
-	
+
 	@GetMapping
-	public Page<TopicDto> list(@RequestParam(required = false) String nameCourse, @RequestParam int page, @RequestParam int size, @RequestParam String ordenation) {
+	public Page<TopicDto> list(@RequestParam(required = false) String nameCourse, @PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable)
+	{
 		Page<Topic> topics;
-		Pageable pageable = PageRequest.of(page, size, Sort.Direction.ASC, ordenation);
-		if (nameCourse == null) {
+		if (nameCourse == null)
+		{
 			topics = topicRepository.findAll(pageable);
-		} else {
-			topics = topicRepository.findByCourse(nameCourse , pageable);
+		}
+		else
+		{
+			topics = topicRepository.findByCourse(nameCourse, pageable);
 		}
 		return TopicDto.converter(topics);
 	}
-	
+
 	@PostMapping
 	@Transactional
-	public ResponseEntity<TopicDto> create(@RequestBody @Valid TopicForm form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicDto> create(@RequestBody @Valid TopicForm form,
+		UriComponentsBuilder uriBuilder)
+	{
 		Topic topic = form.converter(courseRepository);
 		topicRepository.save(topic);
-		
-		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topic.getId()).toUri();
+
+		URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicDto(topic));
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<TopicDetailDto> search(@PathVariable Long id){
+	public ResponseEntity<TopicDetailDto> search(@PathVariable Long id)
+	{
 		Optional<Topic> topic = topicRepository.findById(id);
 		return topic.map(value -> ResponseEntity.ok(new TopicDetailDto(value)))
 			.orElseGet(() -> ResponseEntity.notFound().build());
@@ -63,9 +68,11 @@ public class TopicController
 
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<TopicDto> update(@PathVariable Long id, @RequestBody @Valid UpdateTopicForm form){
+	public ResponseEntity<TopicDto> update(@PathVariable Long id,
+		@RequestBody @Valid UpdateTopicForm form)
+	{
 		Optional<Topic> optional = topicRepository.findById(id);
-		if(optional.isPresent())
+		if (optional.isPresent())
 		{
 			Topic topic = form.update(id, topicRepository);
 			return ResponseEntity.ok(new TopicDto(topic));
@@ -75,9 +82,10 @@ public class TopicController
 
 	@DeleteMapping("/{id}")
 	@Transactional
-	public ResponseEntity<?> delete(@PathVariable Long id){
+	public ResponseEntity<?> delete(@PathVariable Long id)
+	{
 		Optional<Topic> optional = topicRepository.findById(id);
-		if(optional.isPresent())
+		if (optional.isPresent())
 		{
 			topicRepository.deleteById(id);
 			return ResponseEntity.ok().build();
